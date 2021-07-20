0091a2bb7f86c0217f610231e7da993536dfde94
uchansansan <uchansansan@gmail.com>
2021-07-20 17:11:12 +0700

    add P-regulation

--- DuckiebotControl.py
+++ DuckiebotControl.py
@@ -1,33 +1,38 @@
 import numpy as np
 
+
 class DuckiebotControl:
+    k_p = 2.1
+    k_r = 6.0
+    k_d = 2.0
+    k_a = 2.0
+    v_0 = 0.16
     def __init__(self):
-        self.__stop_line_found = False
-
-        self.observation = None
-        self.distance    = 0.0
-        self.angle       = 0.0
-    
-    def __find_stop_line(self):
-        """
-        find stop line on observation
-        and update stop_line_found
-        """
-        self.__stop_line_found = False
-
+        self.distance = 0.0
+        self.angle = 0.0
+        self._previos_distance = 0.0
+        self._previos_angle = 0.0
     def on_stop_line(self):
         return self.__stop_line_found
 
-    def update(self, observation, angle, distance):
-        self.observation = observation
-        self.distance    = distance
-        self.angle       = angle
-
-        self.__stop_line_found()
+    def update(self, angle, distance):
+        self.distance = distance
+        self.angle = angle
 
     def PID_action(self):
         """
         calculate next action
         """
-        action = np.array([0.0, 0.0])
+
+        regul = self.angle
+        dX = self.distance - self._previos_distance
+        dA = self.angle - self._previos_angle
+        v_L = self.v_0 - self.k_p * self.distance
+        v_R = self.v_0 + self.k_p * self.distance
+
+        action = np.array([ (v_R + v_L) / abs(20*regul)  + self.k_a*dA, (v_R - v_L) / 2 + regul*self.k_r*2.1 + self.k_d*dX])
+
+        self._previos_distance = self.distance
+        self._previos_angle = self.angle
+
         return action
