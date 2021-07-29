be178c65d83583d115b7e15b026d8592d64b3f53
uchansansan <uchansansan@gmail.com>
2021-07-29 15:23:53 +0700

    add PD regulation in basic_control.py

--- DuckiebotControl.py
+++ DuckiebotControl.py
@@ -4,35 +4,36 @@ import numpy as np
 class DuckiebotControl:
     k_p = 2.1
     k_r = 6.0
-    k_d = 2.0
-    k_a = 2.0
-    v_0 = 0.16
+    k_d = 2.6
+    k_a = 2.6
+    v_0 = 0.24
+    
     def __init__(self):
         self.distance = 0.0
         self.angle = 0.0
         self._previos_distance = 0.0
         self._previos_angle = 0.0
-    def on_stop_line(self):
-        return self.__stop_line_found
 
     def update(self, angle, distance):
-        self.distance = distance
+        self.distance = distance + 0.05
         self.angle = angle
 
     def PID_action(self):
         """
         calculate next action
         """
-
+        
         regul = self.angle
-        dX = self.distance - self._previos_distance
-        dA = self.angle - self._previos_angle
-        v_L = self.v_0 - self.k_p * self.distance
-        v_R = self.v_0 + self.k_p * self.distance
+        dx = self.distance - self._previos_distance
+        da = self.angle - self._previos_angle
+        v_l = self.v_0 - self.k_p * self.distance - regul * self.k_r - self.k_d * dx - self.k_a * da
+        v_r = self.v_0 + self.k_p * self.distance + regul * self.k_r + self.k_d * dx + self.k_a * da
 
-        action = np.array([ (v_R + v_L) / abs(20*regul)  + self.k_a*dA, (v_R - v_L) / 2 + regul*self.k_r*2.1 + self.k_d*dX])
-
+        #  action = np.array([ (v_R + v_L) / abs(20*regul)  + self.k_a*dA, (v_R - v_L) / 2 + regul*self.k_r*2.1 + self.k_d*dX])  #some magic with angle and rotation
+        
+        action = np.array([(v_r + v_l) / 2, (v_r - v_l) / 2])  # like PD in basic_control
         self._previos_distance = self.distance
         self._previos_angle = self.angle
 
         return action
+    
