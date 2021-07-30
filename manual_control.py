fd5c8ed82f0cae5c6a70d2e673e911d59e38ba36
uchansansan <uchansansan@gmail.com>
2021-07-30 19:34:27 +0700

    Create manual_control.py

---
+++ manual_control.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+# manual
+
+"""
+This script allows you to manually control the simulator or Duckiebot
+using the keyboard arrows.
+"""
+from PIL import Image
+import argparse
+import sys
+from researcher.DuckiebotControl import DuckiebotControl
+from researcher.BarcodeClassifier import BarcodeClassifier
+from cv2 import aruco
+import turtle#ya sosu bibu
+
+import cv2
+
+import gym
+import numpy as np
+import pyglet
+from pyglet.window import key
+
+from gym_duckietown.envs import DuckietownEnv
+
+# from experiments.utils import save_img
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--env-name", default=None)
+parser.add_argument("--map-name", default="udem1")
+parser.add_argument("--distortion", default=False, action="store_true")
+parser.add_argument("--camera_rand", default=False, action="store_true")
+parser.add_argument("--draw-curve", action="store_true", help="draw the lane following curve")
+parser.add_argument("--draw-bbox", action="store_true", help="draw collision detection bounding boxes")
+parser.add_argument("--domain-rand", action="store_true", help="enable domain randomization")
+parser.add_argument("--dynamics_rand", action="store_true", help="enable dynamics randomization")
+parser.add_argument("--frame-skip", default=1, type=int, help="number of frames to skip")
+parser.add_argument("--seed", default=1, type=int, help="seed")
+args = parser.parse_args()
+
+if args.env_name and args.env_name.find("Duckietown") != -1:
+    env = DuckietownEnv(
+        seed=args.seed,
+        map_name=args.map_name,
+        draw_curve=args.draw_curve,
+        draw_bbox=args.draw_bbox,
+        domain_rand=args.domain_rand,
+        frame_skip=args.frame_skip,
+        distortion=args.distortion,
+        camera_rand=args.camera_rand,
+        dynamics_rand=args.dynamics_rand,
+    )
+else:
+    env = gym.make(args.env_name)
+
+env.reset()
+env.render()
+
+
+@env.unwrapped.window.event
+def on_key_press(symbol, modifiers):
+    """
+    This handler processes keyboard commands that
+    control the simulation
+    """
+
+    if symbol == key.BACKSPACE or symbol == key.SLASH:
+        print("RESET")
+        env.reset()
+        env.render()
+    elif symbol == key.PAGEUP:
+        env.unwrapped.cam_angle[0] = 0
+    elif symbol == key.ESCAPE:
+        env.close()
+        sys.exit(0)
+
+    # Take a screenshot
+    # UNCOMMENT IF NEEDED - Skimage dependency
+    # elif symbol == key.RETURN:
+    #     print('saving screenshot')
+    #     img = env.render('rgb_array')
+    #     save_img('screenshot.png', img)
+
+
+# Register a keyboard handler
+key_handler = key.KeyStateHandler()
+env.unwrapped.window.push_handlers(key_handler)
+
+duckiebot = DuckiebotControl()
+barcode   = BarcodeClassifier()
+
+turtle.left(90)
+screen = turtle.Screen()
+screen.title('Map')
+turtle.pensize(1)
+
+start_angle = turtle.heading()
+from_wierd2turtle = 1.1784411471630984
+loop_count = 0
+
+def ka(action):
+    global from_wierd2turtle, bot_pos, bot_heading
+    action *= args.frame_skip
+    turtle.left(action[1]/from_wierd2turtle )
+    turtle.forward(action[0])
+    bot_pos = turtle.pos()
+    bot_heading = turtle.heading()
+
+def tile_calculate(bot_pos):
+    pass
+
+def update(dt):
+    """
+    This function is called at every frame to handle
+    movement/stepping and redrawing
+    """
+    wheel_distance = 0.102
+    min_rad = 0.08
+
+    action = np.array([0.0, 0.0])
+
+    if key_handler[key.UP]:
+        action += np.array([0.44, 0.0])
+    if key_handler[key.DOWN]:
+        action -= np.array([0.44, 0])
+    if key_handler[key.LEFT]:
+        action += np.array([0, 1])
+    if key_handler[key.RIGHT]:
+        action -= np.array([0, 1])
+    if key_handler[key.SPACE]:
+        global start_angle, from_wierd2turtle, loop_count
+        loop_count += 1
+
+        cur_angle = turtle.heading()
+
+        absolute_deffect = 1 - (start_angle - cur_angle)/loop_count/360
+        fw2t = absolute_deffect * from_wierd2turtle
+
+        start_angle = cur_angle
+
+        from_wierd2turtle = fw2t
+        print(fw2t)
+        # action = np.array([0, 0])
+
+    v1 = action[0]
+    v2 = action[1]
+    # Limit radius of curvature
+    if v1 == 0 or abs(v2 / v1) > (min_rad + wheel_distance / 2.0) / (min_rad - wheel_distance / 2.0):
+        # adjust velocities evenly such that condition is fulfilled
+        delta_v = (v2 - v1) / 2 - wheel_distance / (4 * min_rad) * (v1 + v2)
+        v1 += delta_v
+        v2 -= delta_v
+
+    action[0] = v1
+    action[1] = v2
+
+    # Speed boost
+    if key_handler[key.LSHIFT]:
+        action *= 1.5
+
+
+
+
+    lane_pose = env.get_lane_pos2(env.cur_pos, env.cur_angle)
+    distance_to_road_center = lane_pose.dist
+    angle_from_straight_in_rads = lane_pose.angle_rad
+
+    #print(f"{distance_to_road_center=}, {angle_from_straight_in_rads=}")
+
+    global duckiebot, barcode
+    duckiebot.update(angle_from_straight_in_rads, distance_to_road_center)
+    action = duckiebot.PID_action()
+
+
+    obs, reward, done, info = env.step(action)
+    #bot_pos = ()
+    ka(action)
+
+
+    # print("step_count = %s, reward=%.3f" % (env.unwrapped.step_count, reward))
+
+    barcode.update(cv2.cvtColor(obs, cv2.COLOR_RGB2BGR))
+
+    print(*barcode.get_paths(env.cur_angle/np.pi*180 + 180))
+
+    if key_handler[key.RETURN]:
+
+        im = Image.fromarray(obs)
+
+        im.save("screen.png")
+
+    if done:
+        print("done!")
+        env.reset()
+        env.render()
+
+    env.render()
+
+
+pyglet.clock.schedule_interval(update, 1.0 / env.unwrapped.frame_rate)
+
+# Enter main event loop
+pyglet.app.run()
+
+env.close()
